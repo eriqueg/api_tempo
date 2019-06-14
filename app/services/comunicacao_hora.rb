9f862604991010df57dc3ddf6092a6d6cc36fbd2
eriqueg <guizanoni1234@gmail.com>
2019-06-13 22:17:07 -0300

    Fazendo alteração na linguagem de retorno

--- app/services/comunicacao_hora.rb
+++ app/services/comunicacao_hora.rb
@@ -4,11 +4,11 @@ class ComunicacaoHora
         @cidade = cidade
         #@key = 2c95be93f58b3a6e7e8c70e496354c03
 
-        url = "http://api.openweathermap.org/data/2.5/forecast?APPID=2c95be93f58b3a6e7e8c70e496354c03&q=#{@cidade}"
+        url = "http://api.openweathermap.org/data/2.5/forecast/hourly?APPID=2c95be93f58b3a6e7e8c70e496354c03&q=#{@cidade}"
         #url = "https://viacep.com.br/ws/#{@cep}/json/"
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
         #retorno = [:main][:temp]
 
-end
+    end
     
 end
--- app/services/comunicacao_weather.rb
+++ app/services/comunicacao_weather.rb
@@ -6,10 +6,10 @@ class ComunicacaoWeather
             @cidade = cidade
             #@key = 2c95be93f58b3a6e7e8c70e496354c03
 
-            url = "http://api.openweathermap.org/data/2.5/weather?APPID=2c95be93f58b3a6e7e8c70e496354c03&q=#{@cidade}"
+            url = "http://api.openweathermap.org/data/2.5/weather?APPID=2c95be93f58b3a6e7e8c70e496354c03&q=#{@cidade}&lang=pt_br"
             #url = "https://viacep.com.br/ws/#{@cep}/json/"
-            retorno = JSON.parse(Net::HTTP.get(URI(url)))
-            #retorno = [:main][:temp]
+            #retorno = JSON.parse(Net::HTTP.get(URI(url)))
+           retorno = JSON.parse{[:weather[descricao]]}
 
     end
 end
