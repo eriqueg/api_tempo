efca164aadf24ebbbe54f719f3b242f746209b0b
eriqueg <guizanoni1234@gmail.com>
2019-06-20 16:59:42 -0300

    arrumando retorno da previsao horaria

--- app/services/comunicacao_hora.rb
+++ app/services/comunicacao_hora.rb
@@ -2,12 +2,25 @@ class ComunicacaoHora
     def buscar(cidade)
             
         @cidade = cidade
-        #@key = 2c95be93f58b3a6e7e8c70e496354c03
+        
 
-        url = "http://api.openweathermap.org/data/2.5/forecast/hourly?APPID=2c95be93f58b3a6e7e8c70e496354c03&q=#{@cidade}"
-        #url = "https://viacep.com.br/ws/#{@cep}/json/"
+        url = "http://api.openweathermap.org/data/2.5/forecast/hourly?APPID=2c95be93f58b3a6e7e8c70e496354c03&q=#{@cidade}&lang=pt_br"
+        
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
-        #retorno = [:main][:temp]
+        result = []               
+        retorno["list"].each do |horario|           
+            result << {Hora: Time.at(horario["dt"]),
+                            Clima: horario["clouds"]["all"], 
+                            Descrição: horario["weather"][0]["description"],
+                            Temperatura: horario["main"]["temp"],
+                            Pressão: horario["main"]["pressure"],
+                            Humidade: horario["main"]["humidity"],
+                            Temperatura_Mínima: horario["main"]["temp_min"],
+                            Temperatura_Máxima: horario["main"]["temp_max"],
+                            Vento: horario["wind"]["speed"]
+                          }                                                    
+                    end      
+          result
 
     end
     
